29a7336271c7f9547130cecece815429f3571f53
phishchiang <phishchiang@gmail.com>
2022-07-09 09:33:34 +0800

    Clean up the naming.

--- js/shader/render_target_fs.glsl
+++ js/shader/render_target_fs.glsl
@@ -12,20 +12,27 @@ varying vec3 vPosition;
 vec2 distord ( vec2 uv, float progress_val, float zoom ) {
 	vec2 normalized_uv = 2.0 * uv - 1.0; // 0~1 > -1~1
 	vec2 distorted_uv = normalized_uv/(1.0 - progress_val * length(normalized_uv) * zoom);
-
+	/*
+	if progress = 0, everything will be the same
+	if progress = 1, 
+		the further away from the center, the bigger of the value will be
+	*/
 	return (distorted_uv + 1.0) * 0.5;
 }
 
 void main()	{
-	// vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
-	float progress_01 = smoothstep(0.75, 1.0, progress);
-	vec2 new_uv_01 = distord(vUv, -10.0 * progress, 4.0 * progress);
-	vec2 new_uv_02 = distord(vUv, -10.0 * (1.0 - progress), 4.0 * progress);
-	vec4 t1 = texture2D(u_scene_01, new_uv_01);
-	vec4 t2 = texture2D(u_scene_02, new_uv_02);
+	// from original to distorted
+	vec2 distorted_uv_01 = distord(vUv, -10.0 * progress, 4.0 * progress);
+	// from distorted to original
+	vec2 distorted_uv_02 = distord(vUv, -10.0 * (1.0 - progress), 4.0 * progress);
 
-	vec4 t3 = mix(t1, t2, progress_01);
+	vec4 t1 = texture2D(u_scene_01, distorted_uv_01);
+	vec4 t2 = texture2D(u_scene_02, distorted_uv_02);
 
-	gl_FragColor = vec4(progress_01, 0.0,0.0,1.);
+	// General fade in transition
+	float progress_lerp = smoothstep(0.75, 1.0, progress);
+	vec4 t3 = mix(t1, t2, progress_lerp);
+
+	gl_FragColor = vec4(progress_lerp, 0.0,0.0,1.);
 	gl_FragColor = t3;
 }
